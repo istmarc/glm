37ef5699786fd915bc0b0c7fdb149d2034cc51c1
Marc <mnawman@proton.me>
2025-08-13 12:12:01 +0200

    Fit a linear model using Cholesky decomposition

--- source/glm/package.d
+++ source/glm/package.d
@@ -7,31 +7,49 @@ import mir.lapack;
 
 /++
 Backward subtitution
-Solve Ux = y
+Solve Ux = y where U is an upper traingular matrix
 +/
 void backward_subt(T)(Slice!(const(T)*, 2) U, Slice!(const(T)*) y, Slice!(T*) x) {
-	long n = cast(long) x.shape[0];
-	assert(y.elementCount == n);
-	assert(U.shape[0] == n && U.shape[1] == n);
-	x[n-1] = y[n-1] / U[n-1, n-1];
-	for (long i = n-2; i>=0; i--) {
-		T s = y[i];
-		for (long j = i+1; j<n; j++) {
-			s -= U[i, j]* x[j];
-		}
-		x[i] = s / U[i, i];
-	}
+   long n = cast(long) x.elementCount;
+   assert(y.elementCount == n);
+   assert(U.shape[0] == n && U.shape[1] == n);
+   x[n-1] = y[n-1] / U[n-1, n-1];
+   for (long i = n-2; i>=0; i--) {
+      T s = y[i];
+      for (long j = i+1; j<n; j++) {
+         s -= U[i, j]* x[j];
+      }
+      x[i] = s / U[i, i];
+   }
+}
+
+/++
+Forward subtitution
+Solve Lx = y where L is a lower triangular matrix
++/
+void forward_subt(T)(Slice!(const(T)*, 2) L, Slice!(const(T)*) y, Slice!(T*) x) {
+   size_t n = x.elementCount;
+   assert(y.elementCount == n);
+   assert(L.shape[0] == n && L.shape[1] == n);
+   x[0] = y[0] / L[0, 0];
+   for (size_t i = 1; i < n; i++) {
+      T s = y[i];
+      for (size_t j = 0; j < i; j++) {
+      s -= L[i, j] * x[j];
+      }
+      x[i] = s / L[i, i];
+   }
 }
 
 /++ Method for fitting a linear model
-	qr: QR decomposition
-	cholesky: Cholesky decomposition
-	svd: Singular value decomposition
+   qr: QR decomposition
+   cholesky: Cholesky decomposition
+   gd: Gradient descent
 +/
 enum fitMethod{
-	qr,
-	cholesky,
-	svd
+   qr,
+   cholesky,
+   gd
 }
 
 /++
@@ -39,125 +57,166 @@ Linear model.
 +/
 class linearModel(T) {
 private:
-	Slice!(T*) beta;
+   Slice!(T*) beta;
+   bool fitted = false;
 
 public:
-	// Constructor
-	this() {}
+   // Constructor
+   this() {}
 
-	// Destructor
-	~this() {}
+   // Destructor
+   ~this() {}
 
-	// Get the coefficients
-	Slice!(const(T)*) coef() const { return beta;}
+   // Get the coefficients
+   Slice!(const(T)*) coef() const { return beta;}
 
-	/// Fit a linear model where `x` is matrix and `y` is a vector
-	/// By default using qr decomposition
-	void fit(Slice!(const(T)*, 2) x, Slice!(const(T)*) y, fitMethod method = fitMethod.qr) {
-		// x is n*k
-		size_t n = x.shape[0];
-		size_t k = x.shape[1];
-		assert(y.shape[0] == n);
+   /// Fit a linear model where `x` is matrix and `y` is a vector
+   /// By default using qr decomposition
+   void fit(Slice!(const(T)*, 2) x, Slice!(const(T)*) y, fitMethod method = fitMethod.qr) {
+      // x is n*k
+      size_t n = x.shape[0];
+      size_t k = x.shape[1];
+      assert(y.shape[0] == n);
 
-		// X is n*(k+1)
-		auto X = empty!T(n, k+1);
-		X[0..n, 0] = cast(T)(1);
-		X[0..n, 1..k+1] = x;
+      // X is n*(k+1)
+      auto X = empty!T(n, k+1);
+      X[0..n, 0] = cast(T)(1);
+      X[0..n, 1..k+1] = x;
 
-		// Normal equation
-		// XtX is (k+1)*(k+1)
-		auto XtX = empty!T(k+1, k+1);
-		auto Xt = X.transposed();
-		gemm!T(cast(T)(1), Xt, X, cast(T)(0), XtX);
+      // Normal equation
+      // XtX is (k+1)*(k+1)
+      auto XtX = empty!T(k+1, k+1);
+      auto Xt = X.transposed();
+      gemm!T(cast(T)(1), Xt, X, cast(T)(0), XtX);
 
-		// Xty is (k+1)
-		auto Xty = empty!T(k+1);
-		gemv!T(cast(T)(1), Xt, y, cast(T)(0), Xty);
+      // Xty is (k+1)
+      auto Xty = empty!T(k+1);
+      gemv!T(cast(T)(1), Xt, y, cast(T)(0), Xty);
 
-		if (method == fitMethod.qr) {
-			fitLinearModelQR(XtX, Xty);
-		}
-	}
+      if (method == fitMethod.qr) {
+         fitLinearModelQR(XtX, Xty);
+      } else if (method == fitMethod.cholesky) {
+         fitLinearModelCholesky(XtX, Xty);
+      }
+   }
 
-	/// Fit a linear model wehre `x` and `y` are both vectors
-	/// by default using qr decomposition
-	void fit(Slice!(const(T)*) x, Slice!(const(T)*) y, fitMethod method = fitMethod.qr) {
-		size_t n = y.shape[0];
-		assert(n == x.shape[0]);
-		size_t k = 1;
-		// x is n*k = n * 1
-		// X is n*(k+1)
-		auto X = empty!T(n, k+1);
-		X[0..n, 0] = cast(T)(1);
-		X[0..n, 1] = x;
+   /// Fit a linear model wehre `x` and `y` are both vectors
+   /// by default using qr decomposition
+   void fit(Slice!(const(T)*) x, Slice!(const(T)*) y, fitMethod method = fitMethod.qr) {
+      size_t n = y.shape[0];
+      assert(n == x.shape[0]);
+      size_t k = 1;
+      // x is n*k = n * 1
+      // X is n*(k+1)
+      auto X = empty!T(n, k+1);
+      X[0..n, 0] = cast(T)(1);
+      X[0..n, 1] = x;
 
-		// Normal equation
-		// XtX is (k+1)*(k+1)
-		auto XtX = empty!T(k+1, k+1);
-		auto Xt = X.transposed();
-		gemm!T(cast(T)(1), Xt, X, cast(T)(0), XtX);
+      // Normal equation
+      // XtX is (k+1)*(k+1)
+      auto XtX = empty!T(k+1, k+1);
+      auto Xt = X.transposed();
+      gemm!T(cast(T)(1), Xt, X, cast(T)(0), XtX);
 
-		// Xty is (k+1)
-		auto Xty = empty!T(k+1);
-		gemv!T(cast(T)(1), Xt, y, cast(T)(0), Xty);
+      // Xty is (k+1)
+      auto Xty = empty!T(k+1);
+      gemv!T(cast(T)(1), Xt, y, cast(T)(0), Xty);
 
-		if (method == fitMethod.qr) {
-			fitLinearModelQR(XtX, Xty);
-		}
-	}
+      if (method == fitMethod.qr) {
+         fitLinearModelQR(XtX, Xty);
+      } else if (method == fitMethod.cholesky) {
+         fitLinearModelCholesky(XtX, Xty);
+      }
+   }
 
-	Slice!(T*) predict(Slice!(const(T)*) x) {
-		size_t n = x.elementCount;
-		auto X = empty!T(n, 2);
-		X[0..n, 0] = cast(T)(1);
-		X[0..n, 1] = x;
-		auto y = empty!T(n);
-		gemv!T(cast(T)(1), X, beta, cast(T)(0), y);
-		return y;
-	}
+   Slice!(T*) predict(Slice!(const(T)*) x) {
+      assert(fitted, "Model must be fitted before doing predictions");
+      size_t n = x.elementCount;
+      auto X = empty!T(n, 2);
+      X[0..n, 0] = cast(T)(1);
+      X[0..n, 1] = x;
+      auto y = empty!T(n);
+      gemv!T(cast(T)(1), X, beta, cast(T)(0), y);
+      return y;
+   }
 
-	Slice!(T*) predict(Slice!(const(T)*, 2) x) {
-		size_t n = x.shape[0];
-		size_t k = x.shape[1];
-		assert(k + 1 == beta.elementCount);
-		auto X = empty!T(n, k+1);
-		X[0..n, 0] = cast(T)(1);
-		X[0..n, 1..k+1] = x;
-		auto y = empty!T(n);
-		gemv!T(cast(T)(1), X, beta, cast(T)(0), y);
-		return y;
-	}
+   Slice!(T*) predict(Slice!(const(T)*, 2) x) {
+      assert(fitted, "Model must be fitted before doing predictions");
+      size_t n = x.shape[0];
+      size_t k = x.shape[1];
+      assert(k + 1 == beta.elementCount);
+      auto X = empty!T(n, k+1);
+      X[0..n, 0] = cast(T)(1);
+      X[0..n, 1..k+1] = x;
+      auto y = empty!T(n);
+      gemv!T(cast(T)(1), X, beta, cast(T)(0), y);
+      return y;
+   }
 
 private:
-	void fitLinearModelQR(Slice!(const(T)*, 2) XtX, Slice!(const(T)*) Xty) {
-		// Canonical slice kind
-		auto sXtX = empty!T(XtX.shape).canonical;
-		// Copy XtX into sXtX
-		for (size_t i = 0; i < XtX.elementCount; i++) {
-			sXtX.iterator[i] = XtX.iterator[i];
-		}
-		size_t n = XtX.shape[0];
-		assert(XtX.shape[1] == n);
-		auto tau = empty!T([n]).canonical;
-		auto work = empty!T([n]).canonical;
-		auto info1 = geqrf(sXtX, tau, work);
-		assert(info1 == 0);
-		// Copy the upper triangular matrix into R
-		auto R = zeros!T(n, n);
-		foreach(i; 0..n){
-			foreach(j; i..n){
-				R[i,j] = sXtX[j, i];
-			}
-		}
-		auto info2 = orgqr(sXtX, tau, work);
-		assert(info2 == 0);
-		alias Qt = sXtX;
-		// Solve R*beta = b where b=Qt*Xty
-		auto b = empty!T(n);
-		gemv!T(cast(T)(1), Qt, Xty, cast(T)(0), b);
+   // Fit a linear model using QR decomposition
+   void fitLinearModelQR(Slice!(const(T)*, 2) XtX, Slice!(const(T)*) Xty) {
+      // Canonical slice kind
+      auto sXtX = empty!T(XtX.shape).canonical;
+      // Copy XtX into sXtX
+      for (size_t i = 0; i < XtX.elementCount; i++) {
+         sXtX.iterator[i] = XtX.iterator[i];
+      }
+      size_t n = XtX.shape[0];
+      assert(XtX.shape[1] == n);
+      auto tau = empty!T([n]).canonical;
+      auto work = empty!T([n]).canonical;
+      auto info1 = geqrf(sXtX, tau, work);
+      assert(info1 == 0);
+      // Copy the upper triangular matrix into R
+      auto R = zeros!T(n, n);
+      foreach(i; 0..n){
+         foreach(j; i..n){
+            R[i,j] = sXtX[j, i];
+         }
+      }
+      auto info2 = orgqr(sXtX, tau, work);
+      assert(info2 == 0);
+      alias Qt = sXtX;
+      // Solve R*beta = b where b=Qt*Xty
+      auto b = empty!T(n);
+      gemv!T(cast(T)(1), Qt, Xty, cast(T)(0), b);
 
-		beta = empty!T(n);
-		backward_subt(R, b, beta);
-	}
+      beta = empty!T(n);
+      backward_subt(R, b, beta);
+      fitted = true;
+   }
+
+   // Fit a linear model using Cholesky decomposition
+   void fitLinearModelCholesky(Slice!(const(T)*, 2) XtX, Slice!(const(T)*) Xty) {
+      // Canonical slice kind
+      auto sXtX = empty!T(XtX.shape).canonical;
+      // Copy XtX into sXtX
+      for (size_t i = 0; i < XtX.elementCount; i++) {
+         sXtX.iterator[i] = XtX.iterator[i];
+      }
+      size_t n = XtX.shape[0];
+      assert(XtX.shape[1] == n);
+      auto info = potrf!T('L', sXtX);
+      assert(info == 0);
+      auto L = zeros!T(n, n);
+      foreach(i; 0..n) {
+         foreach(j; 0..i+1) {
+            L[i,j] = sXtX[j, i];
+         }
+      }
+      auto z = empty!T(n);
+      forward_subt!T(L, Xty, z);
+      beta = empty!T(n);
+      auto Lt = zeros!T(n, n);
+      foreach(i; 0..n) {
+         foreach(j; 0..i+1) {
+            Lt[j, i] = L[i, j];
+         }
+      }
+      backward_subt!T(Lt, z, beta);
+      fitted = true;
+   }
+
 }
 
--- test/source/app.d
+++ test/source/app.d
@@ -15,49 +15,68 @@ import ggplotd.geom: geomPoint, geomBox, geomLine;
 
 void main()
 {
-	auto x = [1.47f, 1.50f, 1.52f, 1.55f, 1.57f, 1.60f, 1.63f, 1.65f, 1.68f, 1.70f, 1.73f, 1.75f, 1.78f, 1.80f, 1.83f].sliced(15);
-	auto y = [52.21f, 53.12f, 54.48f, 55.84f, 57.20f, 58.57f, 59.93f, 61.29f, 63.11f, 64.47f, 66.28f, 68.10f, 69.92f, 72.19f, 74.46f].sliced(15);
+   auto x = [1.47f, 1.50f, 1.52f, 1.55f, 1.57f, 1.60f, 1.63f, 1.65f, 1.68f, 1.70f, 1.73f, 1.75f, 1.78f, 1.80f, 1.83f].sliced(15);
+   auto y = [52.21f, 53.12f, 54.48f, 55.84f, 57.20f, 58.57f, 59.93f, 61.29f, 63.11f, 64.47f, 66.28f, 68.10f, 69.92f, 72.19f, 74.46f].sliced(15);
 
-	{
-		writeln("y~beta_0 + beta_1 x");
-		auto xs = x.array;
-		auto ys = y.array;
+   {
+      writeln("y~beta_0 + beta_1 x using QR");
+      auto xs = x.array;
+      auto ys = y.array;
 
 
-		auto model = new linearModel!float();
-		model.fit(x, y);
-		auto coef = model.coef();
-		writeln("Fitted coef = ", coef);
+      auto model = new linearModel!float();
+      model.fit(x, y);
+      auto coef = model.coef();
+      writeln("Fitted coef = ", coef);
 
-		auto pred = model.predict(x);
-		writeln("true y    = ", y);
-		writeln("predicted = ", pred);
-		/*
-		// TODO Plot the solution
-		// TODO Plot in the same figure
-		auto a = Aes!(float[], "x", float[], "y")(xs, ys);
-		auto gg1 = geomPoint(a).putIn(GGPlotD());
-		gg1.save("scatter.png");
-		auto b = Aes!(float[], "x", float[], "y")(xs, pred.array);
-		auto gg2 = geomLine(b).putIn(GGPlotD());
-		gg2.save("line.png");
-		*/
-	}
+      auto pred = model.predict(x);
+      writeln("true y    = ", y);
+      writeln("predicted = ", pred);
+      /*
+      // TODO Plot the solution
+      // TODO Plot in the same figure
+      auto a = Aes!(float[], "x", float[], "y")(xs, ys);
+      auto gg1 = geomPoint(a).putIn(GGPlotD());
+      gg1.save("scatter.png");
+      auto b = Aes!(float[], "x", float[], "y")(xs, pred.array);
+      auto gg2 = geomLine(b).putIn(GGPlotD());
+      gg2.save("line.png");
+      */
+   }
 
-	{
-		writeln("y~beta_0 + beta_1 x + beta_2 x^2");
-		auto x2 = x*x;
-		size_t n = x.elementCount;
-		auto X = empty!float([n, 2uL]);
-		X[0..n, 0] = x;
-		X[0..n, 1] = x2;
+   {
+      writeln("y~beta_0 + beta_1 x + beta_2 x^2 using QR");
+      auto x2 = x*x;
+      size_t n = x.elementCount;
+      auto X = empty!float([n, 2uL]);
+      X[0..n, 0] = x;
+      X[0..n, 1] = x2;
 
-		auto model = new linearModel!float();
-		model.fit(X, y);
-		auto coef = model.coef();
-		writeln("Fitted coef = ", coef);
-		auto pred = model.predict(X);
-		writeln("true y    = ", y);
-		writeln("predicted = ", pred);
-	}
+      auto model = new linearModel!float();
+      model.fit(X, y);
+      auto coef = model.coef();
+      writeln("Fitted coef = ", coef);
+      auto pred = model.predict(X);
+      writeln("true y    = ", y);
+      writeln("predicted = ", pred);
+   }
+
+   {
+      writeln("y~beta_0 + beta_1 x + beta_2 x^2 using cholesky");
+      auto x2 = x*x;
+      size_t n = x.elementCount;
+      auto X = empty!float([n, 2uL]);
+      X[0..n, 0] = x;
+      X[0..n, 1] = x2;
+
+      auto model = new linearModel!float();
+      model.fit(X, y, fitMethod.cholesky);
+      auto coef = model.coef();
+      writeln("Fitted coef = ", coef);
+      writeln(X.shape);
+      auto pred = model.predict(X);
+      writeln("true y    = ", y);
+      writeln("predicted = ", pred);
+   }
+
 }
